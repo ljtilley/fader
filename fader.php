4e76e77f29e940570d80b53782ff3d14a7ab1f5d
Luke Tilley <luketilley@gmail.com>
2013-02-05 15:00:14 -0800

    initial commit

---
+++ fader.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Plugin Name: Luke's Custom Fader
+Plugin URI: http://luketilley.com
+Description: There was no simple text fader plugin so I wrote one.
+Version: The Plugin's Version Number: 0.1
+Author: Luke Tilley
+Author URI: http://luketilley.com
+License: GPL2
+*/
+?>
+
+<?php
+/*  Copyright 2013  Luke Tilley  (email : luketilley@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
+
+<?php
+
+function fade($atts, $content = null)
+{
+	$outcomes = explode(',', $content);
+	wp_register_script ('faderjs', plugins_url('js/fader.js', __FILE__));
+	wp_enqueue_script ('faderjs');
+	wp_localize_script ('faderjs', 'outcomes', $outcomes);
+	$return_string .= '<div id="fader">
+		<p>some cool text</p>
+		</div>';
+	return $return_string;
+}
+
+add_shortcode('fader', 'fade');
+?>
---
+++ js/fader.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($){  
+	var i = 0;
+	var fader = $("#fader p");
+	fader.text(outcomes[i]);
+	window.setInterval(function (){
+		fader.fadeOut(800, function () { fader.text(outcomes[i]); });
+		fader.fadeIn(800);
+		if(i < outcomes.length - 1){
+			i++;
+		}
+		else {i = 0;}	
+	},8000);
+}); 
+
